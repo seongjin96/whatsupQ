9d5e48473d010458867b36e8a65c5daf4cb323a7
seongjin96 <w8348@naver.com>
2019-08-28 18:42:19 +0900

    이메일 찾기 구현완료

--- routes/api/auth/findID.js
+++ routes/api/auth/findID.js
@@ -5,8 +5,8 @@ const resMessage = require('../../../module/response/responseMessage');
 const statusCode = require('../../../module/response/statusCode');
 const pool = require('../../../config/dbConfig');
 
-router.get('/', async(req, res) => {
-    try{
+router.get('/', async (req, res) => {
+    try {
         var connection = await pool.getConnection();
         const { birth, phone } = req.query;
         if (!birth || !phone) {
@@ -14,13 +14,19 @@ router.get('/', async(req, res) => {
         } else {
             let query = "SELECT email FROM users WHERE birth = ? AND phone = ?";
             let result = await connection.query(query, [birth, phone]);
-
-            console.log(result);
+            console.log(result[0]);
+            if (!result) {
+                res.status(200).json(utils.successFalse(statusCode.BAD_REQUEST, resMessage.NULL_VALUE)); // 수정 필요
+            } else {
+                const data = result[0];
+                res.status(200).json(utils.successTrue(statusCode.OK, resMessage.READ_SUCCESS, data)); // 수정 필요
+            }
         }
-    } catch{
-
+    } catch (err) {
+        console.log(err);
+        res.status(200).json(utils.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.INTERNAL_SERVER_ERROR));
     } finally {
-
+        connection.release();
     }
 })
 module.exports = router;
