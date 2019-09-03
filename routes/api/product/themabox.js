eb861ccb7056016f69ec65f4335ddcba01378dfc
seongjin96 <w8348@naver.com>
2019-09-03 20:44:33 +0900

    테마박스 구현중

--- routes/api/product/themabox.js
+++ routes/api/product/themabox.js
@@ -5,9 +5,9 @@ const statusCode = require('../../../module/response/statusCode');
 const utils = require('../../../module/response/utils');
 
 // 몽고 DB Schema
-const Package = require('../../../schemas/package');
+const Themabox = require('../../../schemas/themabox');
 
-// 패키지 상품 리스트 보여주기
+// 테마박스 상품 리스트 보여주기
 router.get('/', async (req, res) => {
     try {
         const { category, flag } = req.query;
@@ -16,40 +16,40 @@ router.get('/', async (req, res) => {
         } else {
             // 최신순
             if (flag == 1) {
-                var package = await Package.find({
+                var themabox = await Themabox.find({
                     category: { $in: [category]},
                 }).
                 sort({ created_at: 'desc' })
 
             // 가격 낮은 순
             } else if (flag == 3) {
-                var package = await Package.find({
+                var themabox = await Themabox.find({
                     category: { $in: [category]},
                 }).
                 sort({ price: 'asc' })
 
             // 가격 높은 순
             } else if (flag == 4) {
-                var package = await Package.find({
+                var themabox = await Themabox.find({
                     category: { $in: [category]},
                 }).
                 sort({ price: 'desc' })
             }
 
-            let package_data = [];
-            for(let i = 0; i < package.length; i++) {
-                package_data[i] = {
-                    package_id: package[i]._id,
-                    name: package[i].name,
-                    main_img: package[i].main_img,
-                    price: package[i].price,
-                    saled_price: Math.round(package[i].price * (((100 - package[i].sale_ratio) / 100)) * 0.01) * 100
+            let themabox_data = [];
+            for(let i = 0; i < themabox.length; i++) {
+                themabox_data[i] = {
+                    themabox_id: themabox[i]._id,
+                    name: themabox[i].name,
+                    main_img: themabox[i].main_img,
+                    price: themabox[i].price,
+                    saled_price: Math.round(themabox[i].price * (((100 - themabox[i].sale_ratio) / 100)) * 0.01) * 100
                 }
             }
 
             const data = {
-                package_count: package.length,
-                packages: package_data
+                themabox_count: themabox.length,
+                themaboxes: themabox_data
             }
             
             res.status(200).json(utils.successTrue(statusCode.OK, responseMessage.READ_SUCCESS, data));
